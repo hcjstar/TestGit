2ad072d3343977ec4abf4da25b8c2d0442fdb9f4
eclipse_user <eclipse_email@aliyun.com>
2019-01-27 17:34:23 +0800

    新建提交

---
+++ src/main/java/com/troy/git/TestGit.java
@@ -0,0 +1,24 @@
+/**
+ * Project Name:TestGit
+ * File Name:TestGit.java
+ * Package Name:com.troy.git
+ * Date:2019年1月27日下午4:56:40
+ * Copyright (c) 2019, chenzhou1025@126.com All Rights Reserved.
+ *
+*/
+
+package com.troy.git;
+/**
+ * ClassName:TestGit <br/>
+ * Function: TODO ADD FUNCTION. <br/>
+ * Reason:	 TODO ADD REASON. <br/>
+ * Date:     2019年1月27日 下午4:56:40 <br/>
+ * @author   何朝军
+ * @version  
+ * @since    JDK 1.8
+ * @see 	 
+ */
+public class TestGit {
+
+}
+
